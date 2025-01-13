729061bb5d56e1dfee62410031c9580b4beb3d10
jhenline <jhenline@lucidchart.com>
2025-01-12 18:38:24 -0700

    Auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,41 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from 'expo-router/drawer';
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      //region: "us-east-1",
+      userPoolClientId: "1plkm206kt5gqsbrfmc2bpi6ne",
+      userPoolId: "us-east-1_9x8Ke7jsx",
+    }
+  },
+});
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
-        <Drawer.Screen
-          name="index"
-          options={{
-            drawerLabel: "Home",
-            title: "Home",
-          }}
-        />
-        <Drawer.Screen
-          name="about"
-          options={{
-            drawerLabel: "About",
-            title: "About",
-          }}
-        />
-      </Drawer>
-    </GestureHandlerRootView>
+    <Authenticator.Provider>
+      <Authenticator>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Drawer>
+            <Drawer.Screen
+              name="index"
+              options={{
+                drawerLabel: "Home",
+                title: "Home",
+              }}
+            />
+            <Drawer.Screen
+              name="about"
+              options={{
+                drawerLabel: "About",
+                title: "About",
+              }}
+            />
+          </Drawer>
+        </GestureHandlerRootView>
+      </Authenticator>
+    </Authenticator.Provider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,31 @@
-import { View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { Button, Text, View, StyleSheet } from "react-native";
+import { Link, useNavigation } from "expo-router";
 import GridCarouselView from "@/components/GridCarouselView";
+import { useAuthenticator } from "@aws-amplify/ui-react-native";
+import { useEffect } from "react";
 
 export default function Index() {
+  const { signOut } = useAuthenticator();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View style={{
+          paddingRight: 10,
+        }}>
+          <Button
+            onPress={signOut}
+            title="Sign out"
+          />
+        </View>
+      )
+    })
+  })
+
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>Your Number Collection</Text>
       <GridCarouselView
         numberToCount={
           new Map(
@@ -18,9 +39,6 @@ export default function Index() {
             ])
         }
       ></GridCarouselView>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
     </View>
   );
 }
@@ -31,6 +49,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     justifyContent: "center",
     alignItems: "center",
+  },
+  header: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 20,
   },
   text: {
     color: "#fff",
