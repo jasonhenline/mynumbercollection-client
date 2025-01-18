f7f12e85c56c241dcfd9b23b52a1d6a706298ab5
Jarod Gowgiel <jroddg@gmail.com>
2025-01-18 12:38:25 -0800

    Convert index page to use themes
    Although, the index page always uses dark themes, to match previous
    styles more closely.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { useNavigation } from "expo-router";
 import GridCarouselView from "@/components/GridCarouselView";
 import { useAuthenticator } from "@aws-amplify/ui-react-native";
@@ -7,12 +7,12 @@ import { useData } from "@/DataContext";
 import GrantButtonView from "@/components/GrantButtonView";
 import NewGrantCarouselView from "@/components/NewGrantCarouselView";
 import createApiClient from "@/clients/apiClient";
+import { ActivityIndicator, Button, MD3DarkTheme, PaperProvider, Text, useTheme } from "react-native-paper";
 
 export default function Index() {
   const { signOut } = useAuthenticator();
   const navigation = useNavigation();
   const { user } = useAuthenticator();
-
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -21,35 +21,54 @@ export default function Index() {
         }}>
           <Button
             onPress={signOut}
-            title="Sign out"
-          />
+            mode="outlined"
+          >
+          Sign out
+          </Button>
         </View>
       )
     });
   }, []);
+
+
+  // The landing page intentionally and explicitly uses the "dark" theme for contrast with the
+  // other pages. This means that it is not dynamically styled based on the users dark / light
+  // color preference.
+  const darkTheme = MD3DarkTheme
+  const styles = getStyles(darkTheme.colors.background)
+
+  /** 
+   * Wraps the given element in its own PaperProvider, which detaches it from the regular user
+   * preference for theme color.
+   */
+  const wrapInDarkThemeOverride = (element: React.JSX.Element): React.JSX.Element => {
+    return (
+        <PaperProvider theme={darkTheme}>{element}</PaperProvider>
+    )
+  } 
 
   const [isNewGrantView, setIsNewGrantView] = useState<boolean>(false);
   const [newNumbers, setNewNumbers] = useState<{number: number, isNew: boolean}[]>([]);
   const { numberToCountMap,  isLoading, refreshData } = useData();
 
   if (isLoading) {
-      return (
+      return wrapInDarkThemeOverride(
           <View style={styles.container}>
-              <ActivityIndicator size="large" color="#fff" />
+              <ActivityIndicator size="large" />
           </View>
       );
   }
 
   if (isNewGrantView) {
     if (newNumbers.length === 0) {
-      return (
+      return wrapInDarkThemeOverride(
         <View style={styles.container}>
-          <Text style={styles.header}>No new numbers available</Text>
+          <Text style={darkTheme.fonts.headlineSmall}>No new numbers available</Text>
         </View>
       );
     }
 
-    return (
+    return wrapInDarkThemeOverride(
       <View style={styles.container}>
         <NewGrantCarouselView newNumbers={newNumbers} onBackToGrid={() => setIsNewGrantView(false)} />
       </View>
@@ -72,33 +91,23 @@ export default function Index() {
     setIsNewGrantView(true);
   }
 
-  return (
+  return wrapInDarkThemeOverride(
     <View style={styles.container}>
-      <Text style={styles.header}>Your Number Collection</Text>
+      <Text variant="headlineSmall">Your Number Collection</Text>
       <GridCarouselView numberToCount={numberToCountMap} />
       <GrantButtonView onGetNewNumbersPress={handleGetNewNumbersPress} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const getStyles = (backgroundColor: string) => {
+  return StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
+    backgroundColor,
     justifyContent: "center",
     alignItems: "center",
     gap: 20,
-  },
-  header: {
-    color: "#fff",
-    fontSize: 24,
-  },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  },
-});
+  }
+  });
+}
