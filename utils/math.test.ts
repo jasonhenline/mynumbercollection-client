52fe1b75b6c73ebdc2588389bb71ccbc1a5ac378
jhenline <jhenline@lucidchart.com>
2025-01-27 07:46:13 -0700

    Better organization for isPrime test

--- utils/math.test.ts
+++ utils/math.test.ts
@@ -1,6 +1,6 @@
 import { isPrime } from "./math";
 
-{
+describe("isPrime", () => {
     const primesBelow100 = new Set([
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
         71, 73, 79, 83, 89, 97,
@@ -10,4 +10,4 @@ import { isPrime } from "./math";
     test.each(range)("isPrime(%i)", (n) =>
         expect(isPrime(n)).toEqual(primesBelow100.has(n)),
     );
-}
+});
